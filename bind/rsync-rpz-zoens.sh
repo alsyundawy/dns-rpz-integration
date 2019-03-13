99e7e5df03e6c234c17654041d58a25a2456f845
spirillen <44526987+spirillen@users.noreply.github.com>
2019-03-13 22:38:54 +0100

    Added source links
    fixes issue #4

--- bind/rsync-rpz-zoens.sh
+++ bind/rsync-rpz-zoens.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-rsync -avP whitelist /var/lib/bind/whitelist.mypdns.cloud.hosts
-rsync -avP blacklist /var/lib/bind/rpz.mypdns.cloud.hosts
-rsync -avP youtube /var/lib/bind/youtube.mypdns.cloud.hosts
+rsync -avP https://raw.githubusercontent.com/spirillen/rpz-block-list/master/whitelist.mypdns.cloud.hosts /var/lib/bind/whitelist.mypdns.cloud.hosts
+rsync -avP https://raw.githubusercontent.com/spirillen/rpz-block-list/master/rpz.mypdns.cloud.hosts /var/lib/bind/rpz.mypdns.cloud.hosts
+rsync -avP https://raw.githubusercontent.com/spirillen/rpz-block-list/master/youtube.mypdns.cloud.hosts /var/lib/bind/youtube.mypdns.cloud.hosts
 
 rndc reload whitelist.mypdns.cloud
 rndc reload rpz.mypdns.cloud
